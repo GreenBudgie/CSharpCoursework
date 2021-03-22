d81bb458ad944260b7014663ef29c39586a543da
GreenBudgie <forestengine@yandex.ru>
2021-03-22 18:53:44 +0300

    Add project files.

---
+++ Coursework/Form1.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Coursework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*MSScriptControl.ScriptControl sc = new MSScriptControl.ScriptControl();
+            sc.Language = "VBScript";
+            string expression = "log(2)";
+            object result = sc.Eval(expression);
+            MessageBox.Show(result.ToString());*/
+        }
+    }
+}
